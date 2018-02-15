8cead52d3657a4e4ee5bbb4ed8c293dccef4f542
adonese <mmbusif@student.uofk.edu>
2018-02-15 23:28:40 +0200

    adding a database layer

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+from urllib import parse
+import psycopg2
 
-class Config(object):
-    # ...
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+parse.uses_netloc.append("postgres")
+url = parse.urlparse(os.environ["postgres://pvmwvkgbmpdfvp:123d0a1b1642479e9927da70bd4f180e947e448336bb04dbd3b2d81217a6549d@ec2-54-163-234-99.compute-1.amazonaws.com:5432/d5ceo39rqj5ph3"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
