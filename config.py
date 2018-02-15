d08ef3ada4d113d1045d8b75d68b0f72e956f831
adonese <mmbusif@student.uofk.edu>
2018-02-16 00:13:07 +0200

    something nasty's going on

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 import os
 from urllib import parse
-import psycopg2
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get("postgres://pvmwvkgbmpdfvp:123d0a1b1642479e9927da70bd4f180e947e448336bb04dbd3b2d81217a6549d@ec2-54-163-234-99.compute-1.amazonaws.com:5432/d5ceo39rqj5ph3")
