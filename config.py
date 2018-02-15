72c859f425f6b231734fff53d2c5cee62d6b04f5
adonese <mmbusif@student.uofk.edu>
2018-02-15 23:52:52 +0200

    fixing the database issue. God bless sqlalchemy

--- config.py
+++ config.py
@@ -1,14 +1,8 @@
 import os
 from urllib import parse
 import psycopg2
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-parse.uses_netloc.append("postgres")
-url = parse.urlparse(os.environ["postgres://pvmwvkgbmpdfvp:123d0a1b1642479e9927da70bd4f180e947e448336bb04dbd3b2d81217a6549d@ec2-54-163-234-99.compute-1.amazonaws.com:5432/d5ceo39rqj5ph3"])
-
-conn = psycopg2.connect(
-    database=url.path[1:],
-    user=url.username,
-    password=url.password,
-    host=url.hostname,
-    port=url.port
-)
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("postgres://pvmwvkgbmpdfvp:123d0a1b1642479e9927da70bd4f180e947e448336bb04dbd3b2d81217a6549d@ec2-54-163-234-99.compute-1.amazonaws.com:5432/d5ceo39rqj5ph3")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
